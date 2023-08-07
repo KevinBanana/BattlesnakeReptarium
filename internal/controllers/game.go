7d6cd74668a13763d6d40ec203f6edddd2d1fd65
Kevin Bonanno <59422394+KevinBanana@users.noreply.github.com>
2023-08-06 21:43:24 -0400

    Set up endpoints and health check

--- internal/controllers/game.go
+++ internal/controllers/game.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
@@ -19,4 +20,14 @@ func (g GameController) EndGame(c *gin.Context) {
 	c.JSON(http.StatusInternalServerError, gin.Error{
 		Err: errors.New("not implemented"),
 	})
+}
+
+func (g GameController) CalculateMove(c *gin.Context) {
+	c.JSON(http.StatusInternalServerError, gin.Error{
+		Err: errors.New("not implemented"),
+	})
+}
+
+func (g GameController) Health(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"time": time.Now()})
 }
