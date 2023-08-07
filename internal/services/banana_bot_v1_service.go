ef5c270b9b59660b556be30e75a2f91d43560685
Kevin Bonanno <59422394+KevinBanana@users.noreply.github.com>
2023-08-06 23:41:30 -0400

    refactor and add implementations

--- internal/services/banana_bot_v1_service.go
+++ internal/services/banana_bot_v1_service.go
@@ -1,4 +1,4 @@
-package bots
+package services
 
 import (
 	"context"
@@ -14,4 +14,9 @@ type BananaBotV1Svc struct{}
 
 func NewBananaBotV1Svc() *BananaBotV1Svc {
 	return &BananaBotV1Svc{}
+}
+
+func (svc *BananaBotV1Svc) GetMove(ctx context.Context, game model.Game, turn int, board model.Board, self model.Snake) (model.SnakeAction, error) {
+	panic("implement me")
+	return model.SnakeAction{}, nil
 }
--- internal/services/game_engine_service.go
+++ internal/services/game_engine_service.go
@@ -21,3 +21,13 @@ func NewGameEngineSvc(db repo.DB) *GameEngineSvc {
 		db: db,
 	}
 }
+
+func (svc *GameEngineSvc) StartGame(ctx context.Context, game model.Game, board model.Board, self model.Snake) error {
+	panic("implement me")
+	return nil
+}
+
+func (svc *GameEngineSvc) EndGame(ctx context.Context, game model.Game, turn int, board model.Board, self model.Snake) error {
+	panic("implement me")
+	return nil
+}
