a59650303e83ea67a57d79d6bbdc9093bac78310
Kevin Bonanno <59422394+KevinBanana@users.noreply.github.com>
2023-08-07 00:06:33 -0400

    add game test cases

--- internal/controllers/game.go
+++ internal/controllers/game.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 type GameController struct {
@@ -32,6 +33,9 @@ func createSelectedBot(activeBot string) *services.Bot {
 	switch activeBot {
 	case "banana_bot_v1":
 		botSvc = services.NewBananaBotV1Svc()
+	default:
+		log.Errorf("Cannot set active bot: '%s' not found", activeBot)
+		return nil
 	}
 
 	return &botSvc
@@ -50,6 +54,7 @@ func (g GameController) EndGame(c *gin.Context) {
 }
 
 func (g GameController) CalculateMove(c *gin.Context) {
+	// TODO ensure bot is not nil, otherwise reject request
 	c.JSON(http.StatusInternalServerError, gin.Error{
 		Err: errors.New("not implemented"),
 	})
