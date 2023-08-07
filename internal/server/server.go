0bae3b24f8a5593b5dadcb431dcd79ce6c12c075
Kevin Bonanno <59422394+KevinBanana@users.noreply.github.com>
2023-08-06 23:29:40 -0400

    add creation of game engine

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,31 +5,30 @@ import (
 
 	"BattlesnakeReptarium/internal/config"
 	"BattlesnakeReptarium/internal/controllers"
+	"BattlesnakeReptarium/internal/repo"
 
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
-func NewRouter() *gin.Engine {
+func NewRouter(controller controllers.GameController) *gin.Engine {
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
-	gameEngineGroup := router.Group("")
-	{
-		gameEngine := new(controllers.GameController)
-		gameEngineGroup.POST("/start", gameEngine.StartGame)
-		gameEngineGroup.POST("/end", gameEngine.EndGame)
-		gameEngineGroup.POST("/move", gameEngine.CalculateMove)
-		gameEngineGroup.GET("/", gameEngine.Health)
-	}
+	router.POST("/start", controller.StartGame)
+	router.POST("/end", controller.EndGame)
+	router.POST("/move", controller.CalculateMove)
+	router.GET("/", controller.Health)
 
 	log.Info("Router created")
 	return router
 }
 
 func Init() {
-	r := NewRouter()
+	db := repo.Database{}
+	controller := controllers.NewGameController(&db)
+	r := NewRouter(controller)
 
 	conf := config.GetConfig()
 	listenAddress := fmt.Sprintf("%s:%d", conf.Host, conf.Port)
