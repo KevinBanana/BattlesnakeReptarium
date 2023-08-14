1b3865739f7a09848bfae269dc732124cecbdc33
Kevin Bonanno <59422394+KevinBanana@users.noreply.github.com>
2023-08-13 23:43:09 -0400

    Add argument passing for calculating move

--- internal/controllers/game.go
+++ internal/controllers/game.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 	"time"
 
+	"BattlesnakeReptarium/internal/model"
 	"BattlesnakeReptarium/internal/repo"
 	"BattlesnakeReptarium/internal/services"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
@@ -17,8 +19,12 @@ type GameController struct {
 	gameEngineSvc services.GameEngineSvc
 }
 
+const (
+	bananaBotV1 = "banana_bot_v1"
+)
+
 func NewGameController(db repo.DB, activeBot string) GameController {
-	botSvc := createSelectedBot(activeBot)
+	botSvc := createSelectedBotService(activeBot)
 	gameEngineSvc := services.NewGameEngineSvc(db)
 
 	return GameController{
@@ -27,11 +33,11 @@ func NewGameController(db repo.DB, activeBot string) GameController {
 	}
 }
 
-func createSelectedBot(activeBot string) *services.Bot {
+func createSelectedBotService(activeBot string) *services.Bot {
 	var botSvc services.Bot
 
 	switch activeBot {
-	case "banana_bot_v1":
+	case bananaBotV1:
 		botSvc = services.NewBananaBotV1Svc()
 	default:
 		log.Errorf("Cannot set active bot: '%s' not found", activeBot)
@@ -41,31 +47,59 @@ func createSelectedBot(activeBot string) *services.Bot {
 	return &botSvc
 }
 
-func (g GameController) StartGame(c *gin.Context) {
-	c.JSON(http.StatusInternalServerError, gin.Error{
+func (g GameController) StartGame(ctx *gin.Context) {
+	ctx.JSON(http.StatusInternalServerError, gin.Error{
 		Err: errors.New("not implemented"),
 	})
 }
 
-func (g GameController) EndGame(c *gin.Context) {
-	c.JSON(http.StatusInternalServerError, gin.Error{
+func (g GameController) EndGame(ctx *gin.Context) {
+	ctx.JSON(http.StatusInternalServerError, gin.Error{
 		Err: errors.New("not implemented"),
 	})
 }
 
-func (g GameController) CalculateMove(c *gin.Context) {
+func (g GameController) CalculateMove(ctx *gin.Context) {
 	if g.bot == nil {
-		c.JSON(http.StatusInternalServerError, gin.Error{
+		ctx.JSON(http.StatusInternalServerError, gin.Error{
 			Err: errors.New("bot not set"),
 		})
 		return
 	}
-	
-	c.JSON(http.StatusInternalServerError, gin.Error{
+
+	var err error
+	// Get the game, turn, board, and self from the request
+	var game model.Game
+	if err = bindBody(ctx, &game); err != nil {
+		return
+	}
+	var turn int
+	if err = bindBody(ctx, &turn); err != nil {
+		return
+	}
+	var board model.Board
+	if err = bindBody(ctx, &board); err != nil {
+		return
+	}
+	var selfSnake model.Snake
+	if err = bindBody(ctx, &selfSnake); err != nil {
+		return
+	}
+
+	snakeMove, err := g.bot.CalculateMove(ctx, game, turn, board, selfSnake)
+	ctx.JSON(http.StatusInternalServerError, gin.Error{
 		Err: errors.New("not implemented"),
 	})
 }
 
-func (g GameController) Health(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"time": time.Now()})
+func (g GameController) Health(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{"time": time.Now()})
+}
+
+func bindBody(ctx *gin.Context, obj interface{}) error {
+	if err := ctx.ShouldBindBodyWith(obj, binding.JSON); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return err
+	}
+	return nil
 }
