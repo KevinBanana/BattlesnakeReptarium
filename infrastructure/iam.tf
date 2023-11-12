03c82a4a62c0bdd45440ce20de85102757326791
Kevin Bonanno <59422394+KevinBanana@users.noreply.github.com>
2023-11-11 23:18:53 -0500

    set up iam for elastic beanstalk env

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -26,12 +26,153 @@ resource "aws_iam_policy" "beanstalk_policy" {
     "Version": "2012-10-17",
     "Statement": [
         {
+            "Sid": "AllowCloudformationOperationsOnElasticBeanstalkStacks",
             "Effect": "Allow",
             "Action": [
-                "ecr:*",
-                "cloudtrail:LookupEvents"
+                "cloudformation:*"
             ],
-            "Resource": "*"
+            "Resource": [
+                "arn:aws:cloudformation:*:*:stack/awseb-*",
+                "arn:aws:cloudformation:*:*:stack/eb-*"
+            ]
+        },
+        {
+            "Sid": "AllowDeleteCloudwatchLogGroups",
+            "Effect": "Allow",
+            "Action": [
+                "logs:DeleteLogGroup"
+            ],
+            "Resource": [
+                "arn:aws:logs:*:*:log-group:/aws/elasticbeanstalk*"
+            ]
+        },
+        {
+            "Sid": "AllowECSTagResource",
+            "Effect": "Allow",
+            "Action": [
+                "ecs:TagResource"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "ecs:CreateAction": [
+                        "CreateCluster",
+                        "RegisterTaskDefinition"
+                    ]
+                }
+            }
+        },
+        {
+            "Sid": "AllowS3OperationsOnElasticBeanstalkBuckets",
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::elasticbeanstalk-*",
+                "arn:aws:s3:::elasticbeanstalk-*/*"
+            ]
+        },
+        {
+            "Sid": "AllowLaunchTemplateRunInstances",
+            "Effect": "Allow",
+            "Action": "ec2:RunInstances",
+            "Resource": "*",
+            "Condition": {
+                "ArnLike": {
+                    "ec2:LaunchTemplate": "arn:aws:ec2:*:*:launch-template/*"
+                }
+            }
+        },
+        {
+            "Sid": "AllowELBAddTags",
+            "Effect": "Allow",
+            "Action": [
+                "elasticloadbalancing:AddTags"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "elasticloadbalancing:CreateAction": [
+                        "CreateLoadBalancer"
+                    ]
+                }
+            }
+        },
+        {
+            "Sid": "AllowOperations",
+            "Effect": "Allow",
+            "Action": [
+                "autoscaling:AttachInstances",
+                "autoscaling:CreateAutoScalingGroup",
+                "autoscaling:CreateLaunchConfiguration",
+                "autoscaling:CreateOrUpdateTags",
+                "autoscaling:DeleteLaunchConfiguration",
+                "autoscaling:DeleteAutoScalingGroup",
+                "autoscaling:DeleteScheduledAction",
+                "autoscaling:DescribeAccountLimits",
+                "autoscaling:DescribeAutoScalingGroups",
+                "autoscaling:DescribeAutoScalingInstances",
+                "autoscaling:DescribeLaunchConfigurations",
+                "autoscaling:DescribeLoadBalancers",
+                "autoscaling:DescribeNotificationConfigurations",
+                "autoscaling:DescribeScalingActivities",
+                "autoscaling:DescribeScheduledActions",
+                "autoscaling:DetachInstances",
+                "autoscaling:DeletePolicy",
+                "autoscaling:PutScalingPolicy",
+                "autoscaling:PutScheduledUpdateGroupAction",
+                "autoscaling:PutNotificationConfiguration",
+                "autoscaling:ResumeProcesses",
+                "autoscaling:SetDesiredCapacity",
+                "autoscaling:SuspendProcesses",
+                "autoscaling:TerminateInstanceInAutoScalingGroup",
+                "autoscaling:UpdateAutoScalingGroup",
+                "cloudwatch:PutMetricAlarm",
+                "ec2:*",
+                "ecs:CreateCluster",
+                "ecs:DeleteCluster",
+                "ecs:DescribeClusters",
+                "ecs:RegisterTaskDefinition",
+                "elasticbeanstalk:*",
+                "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+                "elasticloadbalancing:ConfigureHealthCheck",
+                "elasticloadbalancing:CreateLoadBalancer",
+                "elasticloadbalancing:DeleteLoadBalancer",
+                "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+                "elasticloadbalancing:DescribeInstanceHealth",
+                "elasticloadbalancing:DescribeLoadBalancers",
+                "elasticloadbalancing:DescribeTargetHealth",
+                "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+                "elasticloadbalancing:DescribeTargetGroups",
+                "elasticloadbalancing:RegisterTargets",
+                "elasticloadbalancing:DeregisterTargets",
+                "iam:ListRoles",
+                "iam:PassRole",
+                "logs:CreateLogGroup",
+                "logs:PutRetentionPolicy",
+                "logs:DescribeLogGroups",
+                "rds:DescribeDBEngineVersions",
+                "rds:DescribeDBInstances",
+                "rds:DescribeOrderableDBInstanceOptions",
+                "s3:GetObject",
+                "s3:GetObjectAcl",
+                "s3:ListBucket",
+                "sns:CreateTopic",
+                "sns:GetTopicAttributes",
+                "sns:ListSubscriptionsByTopic",
+                "sns:Subscribe",
+                "sns:SetTopicAttributes",
+                "sqs:GetQueueAttributes",
+                "sqs:GetQueueUrl",
+                "codebuild:CreateProject",
+                "codebuild:DeleteProject",
+                "codebuild:BatchGetBuilds",
+                "codebuild:StartBuild"
+            ],
+            "Resource": [
+                "*"
+            ]
         }
     ]
 }
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.aws_region
+  region  = var.aws_region
   profile = "battlesnake-terraform"
   default_tags {
     tags = {
@@ -9,8 +9,30 @@ provider "aws" {
 }
 
 resource "aws_elastic_beanstalk_application" "battlesnake" {
-  name = "battlesnake"
+  name        = "battlesnake"
   description = "All battlesnake environments"
+}
 
+resource "aws_elastic_beanstalk_environment" "preprod_env" {
+  name                = "preprod-battlesnake"
+  application         = aws_elastic_beanstalk_application.battlesnake.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.8.2 running Go 1"
 
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.beanstalk_iam_instance_profile.name
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "ENVIRONMENT"
+    value     = "preprod"
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "PORT"
+    value     = "8080"
+  }
 }
