94f4efc978c0251c662831a79ea3b0247f95c50a
Kevin Bonanno <59422394+KevinBanana@users.noreply.github.com>
2025-02-09 22:08:07 -0500

    Update security.tf

--- infrastructure/prod/security.tf
+++ infrastructure/prod/security.tf
@@ -9,18 +9,18 @@ resource "aws_security_group" "public_traffic" {
 resource "aws_vpc_security_group_ingress_rule" "public_traffic" {
   security_group_id = aws_security_group.public_traffic.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  to_port           = 80
+  from_port         = 8080
+  to_port           = 8080
   ip_protocol       = "tcp"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "public_traffic_2" {
-  security_group_id = aws_security_group.public_traffic.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  to_port           = 443
-  ip_protocol       = "tcp"
-}
+# resource "aws_vpc_security_group_ingress_rule" "public_traffic_2" {
+#   security_group_id = aws_security_group.public_traffic.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 443
+#   to_port           = 443
+#   ip_protocol       = "tcp"
+# }
 
 resource "aws_vpc_security_group_ingress_rule" "ssh_traffic" {
   security_group_id = aws_security_group.public_traffic.id
