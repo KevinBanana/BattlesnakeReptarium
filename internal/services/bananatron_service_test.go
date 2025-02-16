5be074944a48e82085cf786fe61fe2325e351fc0
Kevin Bonanno <59422394+KevinBanana@users.noreply.github.com>
2025-02-15 20:12:44 -0500

    chore: remove commented, replaced tests

--- internal/services/bananatron_service_test.go
+++ internal/services/bananatron_service_test.go
@@ -82,29 +82,6 @@ func TestAdjustWeightsForCavernSize(t *testing.T) {
 	svc := NewBananatronSvc()
 	wg := new(sync.WaitGroup)
 
-	//t.Run("Avoid smaller cavern", func(t *testing.T) {
-	//	options := map[model.Direction]float64{model.UP: 0, model.LEFT: 0, model.DOWN: 0, model.RIGHT: 0}
-	//	snakes := []model.Snake{
-	//		{ID: "enemySnake", Head: model.Coord{X: 1, Y: 2}, Body: []model.Coord{{X: 1, Y: 1}, {X: 1, Y: 2}, {X: 2, Y: 1}}},
-	//		{ID: "selfSnake", Head: model.Coord{X: 0, Y: 1}},
-	//	}
-	//	board := model.Board{
-	//		Height:  3,
-	//		Width:   3,
-	//		Food:    nil,
-	//		Hazards: nil,
-	//		Snakes:  snakes,
-	//	}
-	//
-	//	wg.Add(1)
-	//	svc.adjustWeightsForCavernSize(wg, &options, snakes[1].Head, board)
-	//	wg.Wait()
-	//	want := map[model.Direction]float64{model.UP: 1, model.LEFT: 0, model.DOWN: 3, model.RIGHT: 0}
-	//	if !reflect.DeepEqual(options, want) {
-	//		t.Errorf("adjustWeightsForCavernSize() = %v, want %v", options, want)
-	//	}
-	//})
-
 	tests := map[string]struct {
 		selfHeadCoord model.Coord
 		board         model.Board
@@ -146,29 +123,6 @@ func TestAdjustWeightsForCavernSize(t *testing.T) {
 			}
 		})
 	}
-
-	//t.Run("Avoid larger cavern that has more snakes", func(t *testing.T) {
-	//	options := map[model.Direction]float64{model.UP: 0, model.LEFT: 0, model.DOWN: 0, model.RIGHT: 0}
-	//	snakes := []model.Snake{
-	//		{ID: "enemySnake", Head: model.Coord{X: 1, Y: 2}, Body: []model.Coord{{X: 1, Y: 1}, {X: 1, Y: 2}, {X: 2, Y: 1}}},
-	//		{ID: "selfSnake", Head: model.Coord{X: 0, Y: 1}},
-	//	}
-	//	board := model.Board{
-	//		Height:  3,
-	//		Width:   3,
-	//		Food:    nil,
-	//		Hazards: nil,
-	//		Snakes:  snakes,
-	//	}
-	//
-	//	wg.Add(1)
-	//	svc.adjustWeightsForCavernSize(wg, &options, snakes[0].Head, board)
-	//	wg.Wait()
-	//	want := map[model.Direction]float64{model.UP: -1, model.LEFT: -2, model.DOWN: -2, model.RIGHT: -2}
-	//	if !reflect.DeepEqual(options, want) {
-	//		t.Errorf("adjustWeightsForCavernSize() = %v, want %v", options, want)
-	//	}
-	//})
 }
 
 func TestAdjustWeightsForAvoidingCorneredSnakes(t *testing.T) {
